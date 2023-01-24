e1e6dfc3f8a305a82f1dc1da176fd66a0d66e151
karalyons <kara.lyons@kellogg.northwestern.edu>
2023-01-24 20:12:24 +0000

    variables and strings

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -1,33 +1,46 @@
-# To run this code, be sure your current working directory
-# is the same as where this file is located and then run:
-# ruby 1-data.rb
+# # To run this code, be sure your current working directory
+# # is the same as where this file is located and then run:
+# # ruby 1-data.rb
 
-# In Ruby, there are different *types* of data:
+# # In Ruby, there are different *types* of data:
 
-# Numbers
-puts 5;
-puts 2;
+# # Numbers
+# puts 5;
+# puts 2;
 
-puts 5 + 2;
-puts 5 - 2;
-puts 5 * 2;
-puts 5 / 2;
-puts 5 % 2; #modulus for remainder
+# puts 5 + 2;
+# puts 5 - 2;
+# puts 5 * 2;
+# puts 5 / 2;
+# puts 5 % 2; #modulus for remainder
 
-#puts 5.0 / 2.0;
+# #puts 5.0 / 2.0;
 
-puts 5.0 / 2;
+# puts 5.0 / 2;
 
-puts 5 * (2 + 1);
+# puts 5 * (2 + 1);
  
 # Perform simple math with numbers
 
 # Strings
+# puts "Hello, world!";
 
-# Combine strings together
+# # Combine strings together
+# puts "Tacos are" +" delish";
+# puts "Tacos" * 3;
+# # puts 3 * "Tacos"; does not work
+# puts "Tacos " + 3.to_s;
 
-# Variables
+# # Variables
+# food = "Tacos"
+# quantity = 3
+# puts food * quantity;
 
 # Combine strings and variables
+first_name = "Tom"
+# puts "Hello, " + first_name + "!";
+
+greeting = "Hello, #{first_name}!"
+puts greeting;
 
 # String manipulation
