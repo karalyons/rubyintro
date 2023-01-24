31795c15e66cfcd16820aeb692b3bafd59e36efd
karalyons <kara.lyons@kellogg.northwestern.edu>
2023-01-24 21:53:59 +0000

    hashes

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,15 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {"name" => "Ben",
+        "location" => "Evanston",
+        "status" => "teacher"
+    }
 
+    puts profile
 # Accessing data from the hash
+
+puts profile["name"]
+
 
 # More Complex Hashes
